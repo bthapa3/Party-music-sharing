b5d15f3e5b458ce57551209be1b4481fc71135a7
Bishal <bishal@yourgesture.com>
2021-08-05 00:20:26 -0400

    start

---
+++ frontend/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { component } from "react";
+
+import { render } from "react-dom";
+import { Component } from "react";
+
+import HomePage from "./HomePage";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+import RoomJoinPage from "./RoomjoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p> THis is the homepage</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+        </Switch>
+      </Router>
+    );
+  }
+}
