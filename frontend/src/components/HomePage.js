10c6ea0e45d24d9c9272707bb9892403445ea985
Bishal <bishal@yourgesture.com>
2021-08-09 06:39:19 -0400

    final changes

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,23 +11,75 @@ import {
   Redirect,
 } from "react-router-dom";
 
-import RoomJoinPage from "./RoomjoinPage";
-import CreateRoomPage from "./CreateRoomPage";
-
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      });
+  }
+
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            College Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/">
-            <p> THis is the homepage</p>
-          </Route>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return this.state.roomCode ? (
+                <Redirect to={`/room/${this.state.roomCode}`} />
+              ) : (
+                this.renderHomePage()
+              );
+            }}
+          />
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
---
+++ spotify/credentials.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "8c5d5d487a9a4a80893e90a744f17a67"
+CLIENT_SECRET = "1f0d4957947f43b8b19d11ac4aad17eb"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
